cdde7a48bb4949a927e9836b785760e5d9985bbe
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-06-26 19:30:51 +0200

    Ejercicio 3: FECHAS java.time.LocalDate

--- Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio03.java
+++ Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio03.java
@@ -1,4 +1,5 @@
-
+import java.util.Scanner;
+import java.time.LocalDate; // Date.getYear() deprecado
 
 /* Enunciado:
 1.3. Escribir una aplicación que pida el año actual y el de nacimiento del usuario.
@@ -6,4 +7,14 @@ Debe calcular su edad, suponiendo que en el año en curso el usuario ya ha cumpl
 */
 
 public class Ejercicio03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LocalDate fecha = LocalDate.now(); // SIN NEW -- RECOGE LA FECHA
+        // LocalDate fechaEspecifica = LocalDate.of(2026, 3, 15); // También es un método estático
+        int ano = fecha.getYear(); // Saca el año de la fecha
+        System.out.println("Introduce tu año de nacimiento: \n");
+        int nacimiento = sc.nextInt();
+        System.out.println("Tienes " + (ano - nacimiento) + " años");
+    }
+
 }
