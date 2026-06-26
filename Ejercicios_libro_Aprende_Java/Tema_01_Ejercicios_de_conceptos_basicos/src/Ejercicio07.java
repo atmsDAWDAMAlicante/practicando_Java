1dfd78ec7d094ec1194e2154081050cdbfa2e628
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-06-26 19:51:08 +0200

    Ejercicio 7: redondear floats

--- Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio07.java
+++ Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio07.java
@@ -1,9 +1,18 @@
 
-
+import java.lang.Math;
+import java.util.Scanner;
 /* Enunciado:
 1.7 Sería interesante disponer de un programa que pida como entrada un número decimal
 y lo muestre redondeado al entero más próximo.
 */
 
 public class Ejercicio07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número con decimales: ");
+        float numero = sc.nextFloat();
+        //int entero = (int)numero;
+        int entero = Math.round(numero);
+        System.out.println("El numero entero: " + entero);
+    }
 }
