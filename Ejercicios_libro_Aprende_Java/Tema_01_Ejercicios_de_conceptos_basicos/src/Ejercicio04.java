6c32614b2dd0964eff7d7a8c6e5dbf5f63f030d3
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-06-26 19:36:53 +0200

    Ejercicio 4: short

--- Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio04.java
+++ Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio04.java
@@ -7,4 +7,15 @@ es decir, el valor siguiente al máximo es el valor mínimo y viceversa.
 */
 
 public class Ejercicio04 {
+    public static void main(String[] args) {
+        System.out.println("El tipo short permite almacenar valores comprendidos entre -32.768 y 32.767");
+        short numero = 32767;
+        System.out.println("Se suma 1 a " + numero);
+        numero++;
+        System.out.println("Resultado de la suma: " + numero);
+        System.out.println("Se resta 1 a " + numero);
+        numero--;
+        System.out.println("Resultado de la resta: " + numero);
+        System.out.println("Es cíclico.");
+    }
 }
