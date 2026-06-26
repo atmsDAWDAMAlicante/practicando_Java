9682916827217fe462049e6c534dbce73ca9cdad
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-06-26 19:42:16 +0200

    Ejercicio 5: calcular la media

--- Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio05.java
+++ Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio05.java
@@ -6,4 +6,10 @@ Hay que tener en cuenta que la media puede contener decimales.
 */
 
 public class Ejercicio05 {
+    public static void main(String[] args) {
+        float nota1 = 10;
+        float nota2 = 5;
+        float media = (nota1 + nota2) / 2;
+        System.out.println("La media es: " + media);
+    }
 }
