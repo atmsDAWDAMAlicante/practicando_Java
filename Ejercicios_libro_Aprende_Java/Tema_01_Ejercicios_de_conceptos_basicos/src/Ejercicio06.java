f5e428933ab8bee88d09c0ee8790bc85ce6a122a
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-06-26 19:46:13 +0200

    Ejercicio 6: calcular la media cono división entera

--- Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio06.java
+++ Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_de_conceptos_basicos/src/Ejercicio06.java
@@ -6,4 +6,11 @@ de la media de tres notas decimales.
 */
 
 public class Ejercicio06 {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 5;
+        int media = (nota1 + nota2) / 2; // elimina los decimales al ser enteros
+        System.out.println("La media es: " + media);
+
+    }
 }
