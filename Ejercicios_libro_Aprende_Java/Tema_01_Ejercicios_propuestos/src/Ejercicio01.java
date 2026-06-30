6a9d21e238af4dc8b9ed65960c0e3853081b1bc9
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-06-30 20:01:38 +0200

    Ejercicio 01 - Precio con IVA

--- Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_propuestos/src/Ejercicio01.java
+++ Ejercicios_libro_Aprende_Java/Tema_01_Ejercicios_propuestos/src/Ejercicio01.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 /* Enunciado:
 1.1. Un economista nos ha encargado un programa para realizar cálculos con el IVA.
@@ -7,4 +7,19 @@ Debemos mostrar en pantalla el importe correspondiente al IVA y el total.
 */
 
 public class Ejercicio01 {
+    public static void main(String[] args) {
+        // Entrada de datos
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el precio del producto: ");
+        double precio = sc.nextDouble();
+        System.out.println("Introduce tipo del IVA");
+        double iva = sc.nextDouble();
+
+        // Cálculo
+        double importeIVA = precio * (iva * 0.01);
+        double precioConIva = precio + importeIVA;
+        System.out.println("Precio sin IVA: " + precio + " € + IVA " + iva + "%\n" +
+                "TOTAL: " + precioConIva);
+    }
+
 }
