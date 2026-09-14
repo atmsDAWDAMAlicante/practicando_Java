2454725920411664772b1990e5cdf8e18ad80a46
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2026-09-14 19:45:44 +0200

    Ejercicio 5.2 - Invertir orden de un array

--- Ejercicios_libro_Aprende_Java/Tema_05_Tablas/src/Ejercicio02Tablas.java
+++ Ejercicios_libro_Aprende_Java/Tema_05_Tablas/src/Ejercicio02Tablas.java
@@ -1,9 +1,32 @@
+import java.util.Arrays;
 
-/* Enunciado:
-5.1. Diseñar un programa que solicite al usuario
-que introduzca por teclado 5 números decimales a continuación,
-debe mostrar los números en el mismo orden que se han introducido.
+/* Enunciado: VARIACIÓN - LOS NÚMEROS SERÁN ALEATORIOS
+5.2. Escribir una aplicación que solicite al usuario
+cuántos números desea introducir. A continuación,
+se introducirá por teclado esa cantidad de números enteros,
+y por último, los mostrará en el orden inverso al introducido.
 */
 
 public class Ejercicio02Tablas {
+    public static void main(String[] args) {
+
+        int[] arrayAleatorio = new int[5];
+        int[] arrayResultado = new int[5];
+
+        // Primer bucle que rellena el array con números aleatorios
+        for (int i = 0; i <= 4; i++){
+            arrayAleatorio[i] = (int)(Math.random()*100);
+        }
+
+        System.out.println("Array original: " +  Arrays.toString(arrayAleatorio));
+
+        Arrays.sort(arrayAleatorio); // Ordena el array
+        System.out.println("Array ordenado: " +  Arrays.toString(arrayAleatorio));
+
+        // Bucle que recorre en orden inverso el array y lo almacena en otro
+        for (int j = 4; j >= 0; j--){
+            arrayResultado[4-j] = arrayAleatorio[j];
+        }
+        System.out.println("Array invertido: " +  Arrays.toString(arrayResultado));
+    }
 }
